62fbce93fcc76e13507492e295ebf099a2e1618c
Sneha Singh <127472458+Sneha-Singh21@users.noreply.github.com>
2024-12-24 15:06:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Your frontend URL
+  origin: "https://jobhunt-dxwv.onrender.com", // Your frontend URL
   credentials: true, // Allow credentials (cookies)
 };
 
